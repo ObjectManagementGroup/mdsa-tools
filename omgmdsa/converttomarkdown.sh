312815cd0a9b1db68fed22ed8861fd3654613173
Jason McC. Smith <jason@elementalreasoning.com>
2025-06-18 23:21:51 -0700

    Changed tex cache after conversion

--- omgmdsa/converttomarkdown.sh
+++ omgmdsa/converttomarkdown.sh
@@ -7,17 +7,17 @@
 # We cannot stress this enough. Running it later will *overwrite* your Markdown files 
 # if there are still any of the core tex files in the directory.
 
-mkdir -p ./temptex
+mkdir -p ./archivedTex
 for file in ./[0-9A]*.tex; do
     filesize=$(du -k "${file}" | cut -f1)
     target="${file%.tex}.md"
     if ((filesize > 0)) 
     then
         echo "Converting ${file} to ${target}"
-        # pandoc ${file} -f latex+raw_tex -t markdown -o ${target}
+        pandoc ${file} -f latex+raw_tex -t markdown -o ${target}
     else
         echo "${file} is zero length, touching ${target}"
-        # touch ${target}
+        touch ${target}
     fi
-    # mv ${file} temptex/${file}
+    mv ${file} archivedTex/${file}
 done
